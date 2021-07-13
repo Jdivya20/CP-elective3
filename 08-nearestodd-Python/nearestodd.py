771b51b649d3ecd3fc7d39c568f17846b3f13ab9
Divya <divyarupajandhyala@msitprogram.net>
2021-07-13 19:49:00 +0530

    done

--- 08-nearestodd-Python/nearestodd.py
+++ 08-nearestodd-Python/nearestodd.py
@@ -3,9 +3,24 @@
 # In the case of a tie, return the smaller odd value. 
 # Note that the result must be an int, so nearestOdd(13.0) is the int 13, and not the float 13.0.
 
+def isodd(n):
+	if n%2!=0:
+		return True
+	else:
+		return False
 
 
 def fun_nearestodd(n):
-	return 0
+	n1=int(n)
+	before=n1-1
+	after=n1+1
+	if isodd(before) and isodd(after) and n-before==after-n:
+		return before
+	elif n-before<after-n and isodd(before) and isodd(after):
+		return before
+	elif n-before>after-n and isodd(before) and isodd(after):
+		return after
+	else:
+		return n1
 
 
