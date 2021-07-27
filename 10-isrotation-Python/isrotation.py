a259619295a80d95ed5ff59b81dcd9e97b8bfe24
Divya <divyarupajandhyala@msitprogram.net>
2021-07-27 17:53:39 +0530

    done

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -4,7 +4,29 @@
 # returns True if x is a rotation of the digits of y and False otherwise. For example, 
 # 3412 is a rotation of 1234. Any number 
 # is a rotation of itself.
-
+def rightrotate(x,y):
+    i=0
+    x=str(x)
+    while i<len(x):
+        x=x[1::]+x[0:1]
+        i+=1
+        if int(x)==y:
+            return True
+    return False
+def leftrotate(x,y):
+    i=0
+    x=str(x)
+    while i<len(x):
+        x=x[-1:]+x[0:-1]
+        i+=1
+        if int(x)==y:
+            return True
+    return False
+# print(leftrotate(1234,4321))
 def isrotation(x, y):
 	# Your code goes here
-	pass
+	x=str(x)
+	if rightrotate(x,y) or leftrotate(x,y) or x[::-1]==str(y):
+		return True
+	return False
+print(isrotation(3412, 1234))
