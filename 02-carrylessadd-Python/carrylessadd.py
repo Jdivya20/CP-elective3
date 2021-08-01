2d647ffe3528c73b331ab674099ed51ea886917e
Divya <divyarupajandhyala@msitprogram.net>
2021-08-01 23:18:05 +0530

    done

--- 02-carrylessadd-Python/carrylessadd.py
+++ 02-carrylessadd-Python/carrylessadd.py
@@ -6,5 +6,25 @@
 
 
 def fun_carrylessadd(x, y):
-	return 0
+	if x==0 or y==0:
+		return x+y
+	elif x+y< 10:
+		return x+y
+	elif x<10 or y<10:
+		s=(x+y)-10
+		return s
+	else:
+		s=''
+		# for i in range(len(x)-1):
+		while x>10 and y>10:
+			
+			r1=x%10
+			r2=y%10
+			s1=r1+r2
+			s+=str(s1-10)
+			x=x//10
+			y=y//10
+	s=s[::-1]
+	return int(s)
+print(fun_carrylessadd(121, 0))
 
