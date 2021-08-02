1f2ed0c1b4d9f44eed1a92177fd2b653073fc7e5
Divya <divyarupajandhyala@msitprogram.net>
2021-08-02 12:39:51 +0530

    done

--- 05-happyprimes-Python/happyprimes.py
+++ 05-happyprimes-Python/happyprimes.py
@@ -9,6 +9,77 @@
 # Note: the autograder will grade each of the following functions, so they are required. 
 # However, they also are here specifically because they are just the right helper 
 # functions to make nthHappyNumber(n) easier to write!
+def isprime(n):
+    if n>1:
+        for i in range(2,n):
+            if n%i==0:
+                return False
+        return True
+    return False
+def sumOfSquaresOfDigits(n):
+    s=0
+    while n>=1:
+        r=n%10
+        s+=r**2
+        if n==r==s==1:
+            return n
+        n=n//10
+        if n==0:
+            n=s
+            s=0
+        if n==4:
+            return n
+def ishappynumber(n):
+    # Your code goes here
+    if sumOfSquaresOfDigits(n)==1:
+        return True
+    else:
+        return False
+# print(sumOfSquaresOfDigits(23))
+# print(ishappynumber(1))
+
 def ishappyprimenumber(n):
     # Your code goes here
-    pass
+    if isprime(n) and ishappynumber(n):
+        return True
+    else:
+        return False
+print(ishappyprimenumber(23))
+
+# def isPrime(n):
+#     if n>1:
+#         for i in range(2,n):
+#             if n%i==0:
+#                 return False
+#         else:
+#             return True
+#     return False
+            
+# def ishappynumber(n):
+#     # your code goes here
+#     a=[]
+#     if n<=0:
+#         return False
+#     if n==1:
+#         return True
+#     else:
+#         if n<10:
+#             n=n**2
+#             if n==4:
+#                 return False
+#         while n>1:
+#             if n==4:
+#                 return False
+#             n=str(n)
+#             for i in n:
+#                 a.append(int(i)**2)
+#             n=sum(a)
+#             a=[]
+#         return True
+# def ishappyprimenumber(n):
+#     # Your code goes here
+#     if isPrime(n) and ishappynumber(n):
+#         return True
+#     else:
+#         return False
+# print(ishappyprimenumber(23))
