8c84b56fec861fc51ca88c0300a60ee78111cc77
Divya <divyarupajandhyala@msitprogram.net>
2021-08-02 13:10:18 +0530

    test cases wrongly given

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -3,7 +3,23 @@
 # digit (from 0 to 9) at least once. 309 is the smallest number with this property. 
 # Write the function nthWithProperty309 that takes a non-negative int n and returns 
 # the nth number with Property309.
-
+def property309(n):
+	res=n**5
+	a=[]
+	for i in str(res):
+		a.append(int(i))
+	for x in range(0,10):
+		if x not in a:
+			return False
+	return True
 def nthwithproperty309(n):
 	# Your code goes here
-	pass
+	num=0
+	count=0
+	while count<=n:
+		num+=1
+		if property309(num):
+			count+=1
+		
+	return num
+print(nthwithproperty309(0))
