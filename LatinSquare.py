a655f30803dfbdacbfcc0666cf56bdb617c4a5b2
Divya <divyarupajandhyala@msitprogram.net>
2021-08-04 22:55:35 +0530

    done

--- LatinSquare.py
+++ LatinSquare.py
@@ -9,4 +9,23 @@
 
 def isLatinSquare(lst):
     # Your code goes here...
-    pass
+    a=[]
+    row=len(lst)
+    col=len(lst[0])
+    print(len(set(lst[0])))
+    if row==col:
+        for i in lst:
+            if len(set(i))!=row:
+                return False
+            elif len(set(i))==row:
+                for j in i:
+                    a.append(j)
+                if len(set(a))!=col:
+                    return False
+                
+                    a=[]
+    return True
+print(isLatinSquare([  [ 1, 2, 3, 4 ],
+             [ 2, 1, 4, 3 ],
+             [ 3, 4, 1, 2 ],
+             [ 4, 3, 2, 1 ] ]))
